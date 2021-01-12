a5cd2059460e9a61a3dfe12d5d8e15e04bcf0695
shy <chentulin@163.com>
2021-01-12 11:42:02 +0800

    feat:数组

---
+++ src/Ctl/Helper/ArrayHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ctl\Helper;
+
+/**
+ * the helper for array
+ *
+ * Class ArrayHelper
+ *
+ * @author chentulin
+ */
+class ArrayHelper
+{
+    /**
+     * Notes: 将array的key取出来组成一个数组
+     *
+     * Author: chentulin
+     * DateTime: 2021/1/12 11:33
+     * E-MAIL: <chentulinys@163.com>
+     * @param array $arr
+     * @return array
+     */
+    public function columnArray(array $arr):array
+    {
+        return $arr;
+    }
+}

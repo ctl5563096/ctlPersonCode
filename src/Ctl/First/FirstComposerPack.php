9180e4d64a9d8704ccb3479f5066ea62d1f6b978
shy <chentulin@163.com>
2021-01-12 09:59:58 +0800

    feat:初始化composer包

---
+++ src/Ctl/First/FirstComposerPack.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ctl\First;
+
+class FirstComposerPack{
+    /**
+     * description: first composer code created by chentulin
+     * 
+     * author: chentulin
+     * 
+     * date:2020-01-12
+     */
+    public function first(){
+        echo "Hello Composer!";
+    }
+}
